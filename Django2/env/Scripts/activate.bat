8441aafec217ad83fa28718a19b8706ebaba4284
jose lopes <josemateusmanuellopes22@gmail.com>
2024-04-05 03:41:12 +0100

    env

--- Django2/env/Scripts/activate.bat
+++ Django2/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\josem\OneDrive\Desktop\projecto\Django2\env
+set VIRTUAL_ENV=C:\Users\josem\project_django2\django2\env
 
 if not defined PROMPT set PROMPT=$P$G
 
